adb7040f035f632859508b69108b233a79af913d
Muhammad Raza Master <mrazam110@gmail.com>
2017-07-23 15:27:40 +0500

    initial commit with class 1

---
+++ Class 1/MyApplication/app/src/main/java/com/example/muhammadraza/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.muhammadraza.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.clickMeButton);
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText());
+            }
+        });
+    }
+}
